5e8efb51b8c89916576f4da4b0f77d50faffed30
Nick <rebelexplorer31@gmail.com>
2015-03-04 21:56:22 +0700

    Set enemy location and speed

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.speed = speed;
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 }
 
