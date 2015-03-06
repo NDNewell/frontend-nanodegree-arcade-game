6c35ae3876d0cf9dd28a896d136f0d782f6b3211
Nick <rebelexplorer31@gmail.com>
2015-03-06 12:09:07 +0700

    Change Enemy update function and deleted extra this.x (last position) which resulted in slow progressive movement.

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.x + this.speed * dt;
+    this.x += this.speed * dt;
 
         if(this.x > 505) {
             this.x = 0;
