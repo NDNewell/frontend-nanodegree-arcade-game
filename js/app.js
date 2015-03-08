91eeb2ad975ad5d39c49f8fb0342a128fe811d61
Nick <rebelexplorer31@gmail.com>
2015-03-08 16:35:42 +0700

    Add height width variables for Enemy to collision detection algorithm

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.leftSide = this.x;
+    this.rightSide = this.x + 70;
+    this.top = this.y;
+    this.bottom = this.y + 75;
     this.x += this.speed * dt;
 
     if(this.x > 505) {
@@ -20,10 +24,10 @@ Enemy.prototype.update = function(dt) {
 
 function checkCollisions () {
     allEnemies.forEach(function(enemy) {
-             if(enemy.x < player.rightSide &&
-                enemy.x + 70 > player.leftSide &&
-                enemy.y < player.bottom &&
-                enemy.y + 70 > player.top) {
+             if(enemy.leftSide < player.rightSide &&
+                enemy.rightSide > player.leftSide &&
+                enemy.top < player.bottom &&
+                enemy.bottom > player.top) {
                     console.log('collision!');
                     player.startOver();
                 }
@@ -48,9 +52,9 @@ function Player(x,y) {
 
 Player.prototype.update = function(dt) {
     this.leftSide = this.x;
-    this.rightSide = this.x + 70;
+    this.rightSide = this.x + 60;
     this.top = this.y;
-    this.bottom = this.y + 70;
+    this.bottom = this.y + 60;
 }
 
 
