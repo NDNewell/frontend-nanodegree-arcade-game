755846f4674abb88722e3fa1e1a2c856a90e8599
Nick <rebelexplorer31@gmail.com>
2015-03-08 21:46:45 +0700

    Revert "Revert "Add height width variables for Enemy to collision detection algorithm""
    This reverts commit def4b4711b3091f32097c983f638fb0713627d71.

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.leftSide = this.x;
+    this.rightSide = this.x + 70;
+    this.top = this.y;
+    this.bottom = this.y + 75;
     this.x += this.speed * dt;
 
     if(this.x > 505) {
@@ -28,10 +32,10 @@ evilerEnemy.prototype.constructor = evilerEnemy;
 
 function checkCollisions () {
     allEnemies.forEach(function(enemy) {
-             if(enemy.x < player.rightSide &&
-                enemy.x + 70 > player.leftSide &&
-                enemy.y < player.bottom &&
-                enemy.y + 70 > player.top) {
+             if(enemy.leftSide < player.rightSide &&
+                enemy.rightSide > player.leftSide &&
+                enemy.top < player.bottom &&
+                enemy.bottom > player.top) {
                     console.log('collision!');
                     player.startOver();
                 }
@@ -56,9 +60,9 @@ function Player(x,y) {
 
 Player.prototype.update = function(dt) {
     this.leftSide = this.x;
-    this.rightSide = this.x + 70;
+    this.rightSide = this.x + 60;
     this.top = this.y;
-    this.bottom = this.y + 70;
+    this.bottom = this.y + 60;
 }
 
 
